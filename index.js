b94bc04b6229ed3d54603d316fd5dc0b1ef2e341
rkoko <rinda.ko@gmail.com>
2017-09-12 16:34:33 -0400

    merge sort completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+function findMinAndRemoveSorted(array){
+  return array.shift()
+}
 
+function findMinAndRemove(firstArray, secondArray){
+  let firstMin = firstArray[0]
+  let secondMin = secondArray[0]
+  if (firstMin < secondMin){
+    return findMinAndRemoveSorted(firstArray)
+  }else{
+    return findMinAndRemoveSorted(secondArray)
+  }
+}
+
+function merge(firstArray, secondArray){
+  let sorted = []
+  let currentMin
+  while(firstArray.length != 0 && secondArray.length != 0){
+    currentMin = findMinAndRemove(firstArray, secondArray)
+    sorted.push(currentMin)
+  }
+  return sorted.concat(firstArray).concat(secondArray)
+}
+
+function mergeSort(unsortedArray){
+  let midpoint = unsortedArray.length/2
+  let firstHalf = unsortedArray.slice(0,midpoint)
+  let secondHalf = unsortedArray.slice(midpoint,unsortedArray.length)
+
+  if(unsortedArray.length < 2){
+    return unsortedArray
+  }else{
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+}
